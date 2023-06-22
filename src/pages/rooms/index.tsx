1a3ef1a609f2fea503cf72db02d86241d2797f5b
vvsogi <vvsogi@gmail.com>
2023-06-22 09:04:24 +0000

    백업

--- src/pages/rooms/index.tsx
+++ src/pages/rooms/index.tsx
@@ -1,21 +1,36 @@
-import { ReactElement } from 'react'
-
-// material-ui
-import { Typography } from '@mui/material'
+import React, { HtmlHTMLAttributes, ReactElement, useState } from 'react'
 
 // project imports
 import Layout from 'layout'
 import Page from 'components/ui-component/Page'
 import MainCard from 'ui-component/cards/MainCard'
 import RoomList from 'custom/rooms/roomList'
+import FormControlSelect from 'ui-component/extended/Form/FormControlSelect'
 
-const Rooms = () => (
-    <Page title="Rooms">
-        <MainCard title="Rooms">
-            <RoomList />
-        </MainCard>
-    </Page>
-)
+const currencies = [
+    { value: '', label: 'None' },
+    { value: '1', label: '프로젝트1' }
+]
+
+const Rooms = () => {
+    const [choiceProject, setChoiceProject] = useState(false)
+
+    return (
+        <Page title="Rooms">
+            <MainCard
+                title="Rooms"
+                secondary={
+                    <div style={{ width: '300px' }}>
+                        <FormControlSelect currencies={currencies} captionLabel="ProjectId선택" />
+                    </div>
+                }
+            >
+                {/* {choiceProject ? <RoomList /> : null} */}
+                <RoomList />
+            </MainCard>
+        </Page>
+    )
+}
 
 Rooms.getLayout = function getLayout(page: ReactElement) {
     return <Layout>{page}</Layout>
