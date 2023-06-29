5890864ea41adeba0047865460ee646873e96195
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2023-06-29 10:43:36 +0900

    Add mock rooms api

--- src/components/custom/common/InputFiles.tsx
+++ src/components/custom/common/InputFiles.tsx
@@ -1,5 +1,5 @@
 import { TableCell, TableRow } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { UploaderProps } from 'types/project'
 import { FileUploader } from '../common/FileUploader'
 import { ImagePreview, ThumbnailBox } from '../styles/styled'
@@ -9,13 +9,20 @@ interface Props extends UploaderProps {
     thumbnailUrl: string
 }
 
+function cacheRemove(url: string) {
+    if (url.split(':')[0] === 'blob') {
+        return url
+    }
+    return `${url}?timestamp=${Date.now()}`
+}
+
 export function InputFiles({ htmlFor, tableName, thumbnailUrl, setFile, setThumbnailUrl }: Props) {
     return (
         <TableRow>
             <TableCell>{tableName}</TableCell>
             <TableCell>
                 <ThumbnailBox>
-                    <ImagePreview src={thumbnailUrl} alt="Favicon Preview" />
+                    <ImagePreview src={cacheRemove(thumbnailUrl)} alt="Favicon Preview" />
                     <FileUploader htmlFor={htmlFor} setFile={setFile} setThumbnailUrl={setThumbnailUrl} />
                 </ThumbnailBox>
             </TableCell>
