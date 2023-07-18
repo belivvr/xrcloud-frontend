dad3f156bd9a59c63fd4cda9129406fa8729992f
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2023-07-18 17:24:09 +0900

    change name choiced provider and add api docs

--- src/contexts/ChoicedProjectContext.tsx
+++ src/contexts/ChoicedProjectContext.tsx
@@ -2,17 +2,13 @@ import React, { createContext, useState } from 'react'
 import { ProjectContextType } from 'types/auth'
 import { Project } from 'types/project'
 
-const ChoicedProjectContext = createContext<ProjectContextType | null>(null)
+const Context = createContext<ProjectContextType | null>(null)
 
-export function ProjectChoicedProjectProvider({ children }: { children: React.ReactElement }) {
+export function ChoicedProjectProvider({ children }: { children: React.ReactElement }) {
     const [choicedProject, setChoicedProject] = useState<Project>()
     const [choicedScene, setChoicedScene] = useState<string>()
 
-    return (
-        <ChoicedProjectContext.Provider value={{ choicedProject, choicedScene, setChoicedProject, setChoicedScene }}>
-            {children}
-        </ChoicedProjectContext.Provider>
-    )
+    return <Context.Provider value={{ choicedProject, choicedScene, setChoicedProject, setChoicedScene }}>{children}</Context.Provider>
 }
 
-export default ChoicedProjectContext
+export default Context
