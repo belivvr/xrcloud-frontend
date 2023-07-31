49dbe2e998d6be92c8bf74cbeaeb3769736f824e
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2023-07-31 16:12:45 +0900

    Add og tag, Ga tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,42 +5,38 @@ export default function Document() {
         <Html>
             <Head>
                 <meta name="theme-color" content="#2296f3" />
-                <meta name="title" content="Developers XRCLOUD" />
-                <meta
-                    name="description"
-                    content="Start your next React project with Berry admin template. It build with Reactjs, Material-UI, Redux, and Hook for faster web development."
-                />
+                <meta name="title" content="XRCLOUD - BELIVVR" />
+                <meta name="description" content="Homepage Development Costs 3D Spatial Web, Metaverse Services" />
                 <meta
                     name="keywords"
-                    content="react admin template, material-ui react dashboard template, reactjs admin template, reactjs dashboard, react backend template"
+                    content="METAVERSE, CLOUD, PAAS, SAAS, SERVICE, PLATFORM, TECHNOLOGY, Web, WebXR, VR, Internet, 3D Web, 메타버스,  클라우드, 플랫폼, 파스, 서비스, 기술, 웹, 웹XR, VR, 인터넷, 3D web"
                 />
                 <meta property="og:locale" content="en_US" />
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://berrydashboard.io/" />
-                <meta property="og:site_name" content="berrydashboard.io" />
-                <meta property="article:publisher" content="https://www.facebook.com/codedthemes" />
-                <meta property="og:title" content="Berry - React Material Dashboard Template" />
-                <meta
-                    property="og:description"
-                    content="Berry Dashboard is made for the faster web application development built using Material-UI, Reactjs, Redux & Hook API."
-                />
-                <meta property="og:image" content="https://berrydashboard.io/og-image/og-facebook.png" />
+                <meta property="og:url" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
+                <meta property="og:site_name" content="XRCLOUD - BELIVVR" />
+                <meta property="og:title" content="XRCLOUD - BELIVVR" />
+                <meta property="og:description" content="Homepage Development Costs 3D Spatial Web, Metaverse Services" />
+                <meta property="og:image" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://berrydashboard.io" />
-                <meta property="twitter:title" content="Berry - React Material Dashboard Template" />
-                <meta
-                    property="twitter:description"
-                    content="Berry Dashboard is made for the faster web application development built using Material-UI, Reactjs, Redux & Hook API."
-                />
-                <meta property="twitter:image" content="https://berrydashboard.io/og-image/og-twitter.png" />
-                <meta name="twitter:creator" content="@codedthemes" />
+                <meta property="twitter:url" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
+                <meta property="twitter:title" content="XRCLOUD - BELIVVR" />
+                <meta property="twitter:description" content="Homepage Development Costs 3D Spatial Web, Metaverse Services" />
+                <meta property="twitter:image" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
 
-                <link rel="icon" href="/favicon.svg" />
+                <link rel="icon" href="https://uploads-ssl.webflow.com/6449cc542956b39c2cb4660c/644e22096aae422abdb75902_icon_32x32.png" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
                     rel="stylesheet"
                 />
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-E822WDDXP0"></script>
+                <script>
+                    {`  window.dataLayer = window.dataLayer || [];
+                                                function gtag(){dataLayer.push(arguments);}
+                                                gtag('js', new Date());
+                                                gtag('config', 'G-E822WDDXP0');`}
+                </script>
             </Head>
             <body>
                 <Main />
