98b7abfbae0408eb2649d0f4138ff4670ebb00c9
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2023-08-10 18:41:02 +0900

    test meta tag

--- src/components/ui-component/Page.tsx
+++ src/components/ui-component/Page.tsx
@@ -14,37 +14,25 @@ interface Props extends BoxProps {
     title: string
 }
 
-const Page = forwardRef<HTMLDivElement, Props>(({ children, title = '', meta, ...other }: Props, ref: Ref<HTMLDivElement>) => (
-    <>
-        <Head>
-            <title>{`${title} | XRCLOUD`}</title>
-            <meta name="title" content="XRCLOUD - BELIVVR" />
-            <meta name="description" content="Homepage Development Costs 3D Spatial Web, Metaverse Services" />
-            <meta
-                name="keywords"
-                content="METAVERSE, CLOUD, PAAS, SAAS, SERVICE, PLATFORM, TECHNOLOGY, Web, WebXR, VR, Internet, 3D Web, 메타버스,  클라우드, 플랫폼, 파스, 서비스, 기술, 웹, 웹XR, VR, 인터넷, 3D web"
-            />
-            <meta property="og:locale" content="en_US" />
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
-            <meta property="og:site_name" content="XRCLOUD - BELIVVR" />
-            <meta property="og:title" content="XRCLOUD - BELIVVR" />
-            <meta property="og:description" content="Homepage Development Costs 3D Spatial Web, Metaverse Services" />
-            <meta property="og:image" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
-            <meta property="twitter:title" content="XRCLOUD - BELIVVR" />
-            <meta property="twitter:description" content="Homepage Development Costs 3D Spatial Web, Metaverse Services" />
-            <meta property="twitter:image" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
+const Page = forwardRef<HTMLDivElement, Props>(({ children, title = '', meta, ...other }: Props, ref: Ref<HTMLDivElement>) => {
+    return (
+        <>
+            <Head>
+                {meta}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
+                <meta property="og:image" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
+                <meta property="twitter:image" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
+                <link rel="icon" href="https://uploads-ssl.webflow.com/6449cc542956b39c2cb4660c/644e22096aae422abdb75902_icon_32x32.png" />
+            </Head>
 
-            <link rel="icon" href="https://uploads-ssl.webflow.com/6449cc542956b39c2cb4660c/644e22096aae422abdb75902_icon_32x32.png" />
-            {meta}
-        </Head>
-
-        <Box ref={ref} {...other}>
-            {children}
-        </Box>
-    </>
-))
+            <Box ref={ref} {...other}>
+                {children}
+            </Box>
+        </>
+    )
+})
 
 export default Page
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { useLocalization } from 'hooks/useLocalization'
 import MainCard from 'ui-component/cards/MainCard'
 import fs from 'fs'
 import { MarkdownView } from 'components/custom/common/MarkdownView'
+import Head from 'next/head'
 
 type Locale = 'en' | 'ko'
 
@@ -17,11 +18,51 @@ interface StaticProps {
     defaultLocale: Locale
 }
 
-export async function getStaticProps(data: StaticProps) {
+interface Metatag {
+    title: string
+    description: string
+    keywords: string
+    locale: string
+}
+
+interface Props {
+    post: string
+    locale: Locale
+    meta: Metatag
+}
+
+export const getServerSideProps = async (data: StaticProps) => {
     try {
         const post = fs.readFileSync(`public/landing-${data.locale}.md`).toString()
+        if (data.locale === 'ko') {
+            const meta: Metatag = {
+                title: 'XRCLOUD:웹 메타버스 서비스 개발을 위한 클라우드 SaaS',
+                description: `저렴한 비용, 빠른 접근, 강력한 기능의 3D공간 웹 메타버스를 만들 때는 XRCLOUD를 이용하세요.
+                홈페이지를 만드는 것 처럼 쉽고 빠르게 적은 비용으로도 웹 메타버스를 만들 수 있습니다.
+                글로벌 메타버스 오픈소스 모질라허브에 개발,운영에 필요한 추가 기능을 더하고
+                공공기관을 위해  안전한 G클라우드 까지 제공합니다. XRCLOUD로 XR공간 컴퓨팅 시대의 웹 서비스를 준비하세요.`,
+                keywords: '메타버스, 웹메타버스, 모질라허브, XRCLOUD, G클라우드, WebXR, 공간웹',
+                locale: 'ko-KR'
+            }
+            return {
+                props: {
+                    post,
+                    locale: data.locale,
+                    meta
+                }
+            }
+        }
+        const meta: Metatag = {
+            title: 'XRCLOUD:SaaS for Web Metaverse Service Develop',
+            description: `Use XRCLOUD to create a low-cost, easy-to-access, powerful features 3D space web metaverse.
+            You can create a website bus faster and cheaper like making a homepage.
+            It provides all the features of the proven Mozilla Hub opensource, and provides additional features necessary for site operation and development.
+            Prepare your web services for the XR Spatial Computing era with XRCLOUD.`,
+            keywords: 'Metaverse, Web Metaverse, XRCLOUD, Metaverse Saas, WebXR, Spatial Web',
+            locale: 'en-US'
+        }
         return {
-            props: { post, locale: data.locale }
+            props: { post, locale: data.locale, meta }
         }
     } catch (err) {
         console.error(err)
@@ -32,7 +73,7 @@ export async function getStaticProps(data: StaticProps) {
     }
 }
 
-const Landing = ({ post, locale }: { post: string; locale: Locale }) => {
+const Landing = ({ post, locale, meta }: Props) => {
     const [markdown, setMarkdown] = useState<string>(post)
     const { onChangePresetColor, locale: configLocale, onChangeLocale } = useConfig()
     const localization = useLocalization(locale)
@@ -53,6 +94,17 @@ const Landing = ({ post, locale }: { post: string; locale: Locale }) => {
 
     return (
         <Page position={'relative'} title={localization.landing}>
+            <Head>
+                <title>{meta.title}</title>
+                <meta name="title" content={meta.title} />
+                <meta name="description" content={meta.description} />
+                <meta name="keywords" content={meta.keywords} />
+                <meta property="og:locale" content={meta.locale} />
+                <meta property="og:title" content={meta.title} />
+                <meta property="og:description" content={meta.description} />
+                <meta property="twitter:title" content={meta.title} />
+                <meta property="twitter:description" content={meta.description} />
+            </Head>
             <MainCard title={localization.landing}>
                 <MarkdownView post={markdown} />
             </MainCard>
