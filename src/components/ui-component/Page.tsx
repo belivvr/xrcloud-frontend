89b341531eec5183455c41fb68fab90aa1ea8561
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2023-08-21 10:36:00 +0900

    fix images og tag in page.tsx

--- src/components/ui-component/Page.tsx
+++ src/components/ui-component/Page.tsx
@@ -20,11 +20,7 @@ const Page = forwardRef<HTMLDivElement, Props>(({ children, title = '', meta, ..
             <Head>
                 {meta}
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
-                <meta property="og:image" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
-                <meta property="twitter:image" content="https://kr.object.ncloudstorage.com/xrcloud-prod-backend/belivvr.png" />
                 <link rel="icon" href="https://uploads-ssl.webflow.com/6449cc542956b39c2cb4660c/644e22096aae422abdb75902_icon_32x32.png" />
             </Head>
 
