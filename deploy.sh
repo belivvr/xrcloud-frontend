38dda65bf421f7a719af28f100491b223203539f
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2023-09-01 18:41:15 +0900

    fix deploy sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git pull
 docker build -t frontend .
 docker system prune -f
 if ! docker network ls | grep -q xrcloud; then
-   docker network create xrcloud
+   docker network create --subnet=172.18.0.0/16 xrcloud
 fi
 docker rm -f frontend
 docker run -d --restart=always --name frontend --network xrcloud frontend
