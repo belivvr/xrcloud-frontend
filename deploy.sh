e0159e0833d28d52c2ee382b495955bef11c9f38
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2023-09-01 16:19:34 +0900

    Add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
+cd /home/benny/xrcloud-frontend
+
 git pull
 docker build -t frontend .
+docker system prune -f
 docker rm -f frontend
-docker run -d --restart=always --name frontend --network xrcloud frontend
+docker run -d --restart=always --name frontend --network suncheon frontend
 docker logs frontend -f
