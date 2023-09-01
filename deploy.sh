77fa480bfcfcde0000e19776ec0c192ef602e241
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2023-09-01 17:19:08 +0900

    fix deploy (add docker network)

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@ cd /home/benny/xrcloud-frontend
 git pull
 docker build -t frontend .
 docker system prune -f
+if ! docker network ls | grep -q xrcloud; then
+   docker network create xrcloud
+fi
 docker rm -f frontend
 docker run -d --restart=always --name frontend --network xrcloud frontend
 docker logs frontend -f
