8f07bf39196b4e0ca34e943f39afba3e3bbd2a9e
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 <luke.yang@belivvr.com>
2024-09-27 16:09:03 +0900

    com

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,6 +3,7 @@
 module.exports = {
     i18n: {
         defaultLocale: 'en',
-        locales: ['en', 'ko']
+        locales: ['en', 'ko'],
+        localeDetection: true // 로케일 자동 감지 활성화
     }
 }
